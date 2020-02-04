e0b8194d654ce21cc10ec1902fac63bea397c917
alex <alexctrx@gmail.com>
2020-02-04 12:31:31 +0000

    Initial commit

---
+++ src/main/java/com/alexcatarau/hba/HbaApplication.java
@@ -0,0 +1,13 @@
+package com.alexcatarau.hba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbaApplication.class, args);
+	}
+
+}
