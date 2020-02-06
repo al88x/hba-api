6b885230ab0ce9f3b74bb8ec93c0026f04fcc417
Alexandru Catarau <alexctrx@gmail.com>
2020-02-06 13:48:15 +0000

    Changed maven surefire dependency

--- src/test/java/com/alexcatarau/hba/LoginIntegrationTest.java
+++ src/test/java/com/alexcatarau/hba/LoginIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.alexcatarau.hba.security;
+package com.alexcatarau.hba;
 
 
 import com.alexcatarau.hba.config.IntegrationTestConfig;
@@ -8,9 +8,11 @@ import org.jdbi.v3.core.Jdbi;
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
